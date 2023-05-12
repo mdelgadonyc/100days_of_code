451d6c903ad78b7300567b15b08f1abf821b88e6
mdelgadonyc <moises.f.delgado@gmail.com>
2023-05-12 17:30:43 -0400

    Adds day 12 (guessing) project

---
+++ project_day_012/guess.py
@@ -0,0 +1,31 @@
+import random
+from art import logo
+
+guesses = 0
+
+print(logo)
+
+print("Welcome to the Number Guessing Game!")
+
+# generate random number between 1 and 100
+secret_num = random.randint(1, 100)
+print("I'm thinking of a number between 1 and 100.")
+
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
+if difficulty == 'easy':
+    guesses = 10
+elif difficulty == 'hard':
+    guesses = 5
+
+while (guesses > 0):
+    print(f"You have {guesses} attempts remaining to guess the number.")
+    my_guess = int(input("Make a guess: "))
+    if my_guess == secret_num:
+        # hit!
+        print(f"You got it! The answer was {secret_num}.")
+        break
+    elif my_guess > secret_num:
+        print("Too high.")
+    else:
+        print("Too low.")
+    guesses -= 1
