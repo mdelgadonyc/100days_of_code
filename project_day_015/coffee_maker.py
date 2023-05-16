ea1473f1c5fc2e6356452237086742308d9fded5
mdelgadonyc <moises.f.delgado@gmail.com>
2023-05-15 21:16:48 -0400

    Removes unnecessary comments

--- project_day_015/coffee_maker.py
+++ project_day_015/coffee_maker.py
@@ -35,13 +35,6 @@ def turn_machine_off():
     machine_on = False
 
 
-# 3. Print report.
-# a. When the user enters “report” to the prompt, a report should be generated that shows
-# the current resource values. e.g.
-# Water: 100ml
-# Milk: 50ml
-# Coffee: 76g
-# Money: $2.5
 def generate_report():
     print(f"water level: {resources_level['water']}ml")
     print(f"milk level: {resources_level['milk']}ml")
@@ -50,11 +43,6 @@ def generate_report():
 
 
 # 4. Check resources sufficient?
-# a. When the user chooses a drink, the program should check if there are enough
-# resources to make that drink.
-# b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
-# not continue to make the drink but print: “Sorry there is not enough water.”
-# c. The same should happen if another resource is depleted, e.g. milk or coffee.
 def check_resources(beverage):
     if BEVERAGE_MENU[beverage]["water"] > resources_level["water"]:
         print("Sorry there is not enough water.")
@@ -81,6 +69,22 @@ def request_payment():
 
 def serve_beverage(coffee_choice):
     resources_level['money'] += BEVERAGE_MENU[coffee_choice]["price"]
+
+    # TODO 7. Make Coffee.
+    # a. If the transaction is successful and there are enough resources to make the drink the
+    # user selected, then the ingredients to make the drink should be deducted from the
+    # coffee machine resources.
+    # E.g. report before purchasing latte:
+    # Water: 300ml
+    # Milk: 200ml
+    # Coffee: 100g
+    # Money: $0
+    # Report after purchasing latte:
+    # Water: 100ml
+    # Milk: 50ml
+    # Coffee: 76g
+    # Money: $2.5
+
     print(f"Here is your latte 🍵 Enjoy!")
 
 
@@ -88,7 +92,6 @@ def start_machine():
 
     while machine_on:
 
-        # TODO 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
         coffee_choice = input("What would you like? (espresso/latte/cappuccino): ")
         if coffee_choice in BEVERAGE_MENU:
             if check_resources(coffee_choice):
@@ -100,63 +103,11 @@ def start_machine():
                     serve_beverage(coffee_choice)
                 else:
                     print(f"Sorry that's not enough money. Money refunded.")
-
-        # TODO 2. Turn off the Coffee Machine by entering “off” to the prompt.
+        # Turn off the Coffee Machine by entering “off” to the prompt.
         elif coffee_choice == "off":
             turn_machine_off()
         elif coffee_choice == "report":
             generate_report()
 
-# a. Check the user’s input to decide what to do next.
-# b. The prompt should show every time action has completed, e.g. once the drink is dispensed.
-# The prompt should show again to serve the next customer.
-
 
 start_machine()
-
-
-"""
-Coffee Machine Program Requirements
-
-4. Check resources sufficient?
-a. When the user chooses a drink, the program should check if there are enough
-resources to make that drink.
-b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
-not continue to make the drink but print: “Sorry there is not enough water.”
-c. The same should happen if another resource is depleted, e.g. milk or coffee.
-5. Process coins.
-a. If there are sufficient resources to make the drink selected, then the program should
-prompt the user to insert coins.
-b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
-pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
-6. Check transaction successful?
-a. Check that the user has inserted enough money to purchase the drink they selected.
-E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
-program should say “Sorry that's not enough money. Money refunded.”.
-b. But if the user has inserted enough money, then the cost of the drink gets added to the
-machine as the profit and this will be reflected the next time “report” is triggered. E.g.
-Water: 100ml
-Milk: 50ml
-Coffee: 76g
-Money: $2.5
-c. If the user has inserted too much money, the machine should offer change.
-E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal
-places.
-7. Make Coffee.
-a. If the transaction is successful and there are enough resources to make the drink the
-user selected, then the ingredients to make the drink should be deducted from the
-coffee machine resources.
-E.g. report before purchasing latte:
-Water: 300ml
-Milk: 200ml
-Coffee: 100g
-Money: $0
-Report after purchasing latte:
-Water: 100ml
-Milk: 50ml
-Coffee: 76g
-Money: $2.5
-b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If
-latte was their choice of drink.
-"""
