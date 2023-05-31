41eae48922e77244143a840c794cf98319c3d3cf
mdelgadonyc <moises.f.delgado@gmail.com>
2023-05-31 17:16:32 -0400

    Adds car with player collision detection

--- capstone-002/main.py
+++ capstone-002/main.py
@@ -21,8 +21,6 @@ screen.onkey(tommy.move, "Up")
 car_manager = CarManager()
 car_manager.new_car()
 
-# TODO: Detect when the turtle player collides with a car and stop the game if this happens.
-
 # TODO: Detect when the turtle player has reached the top edge of the screen (i.e., reached the FINISH_LINE_Y). When
 #  this happens, return the turtle to the starting position and increase the speed of the cars. Hint: think about
 #  creating an attribute and using the MOVE_INCREMENT to increase the car speed.
@@ -37,6 +35,11 @@ while game_is_on:
     time.sleep(0.1)
     car_manager.new_car()
     car_manager.move_cars()
+
+    # Detect when the turtle player collides with a car and stop the game if this happens.
+    for car in car_manager.cars:
+        if car.distance(tommy) < 20:
+            game_is_on = False
 
     screen.update()
 
