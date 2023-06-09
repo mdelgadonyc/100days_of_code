29e3a84d1b04d605fe6fd1b453bc08ac4c748753
mdelgadonyc <moises.f.delgado@gmail.com>
2023-06-09 19:51:43 -0400

    Adds UI elements to day 28 project

--- project_day_028/main.py
+++ project_day_028/main.py
@@ -1,4 +1,6 @@
 
+from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -16,3 +18,31 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+# Top "Timer" label
+timer_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 40, "bold"))
+timer_label.grid(column=1, row=0)
+
+# Canvas with tomato image and countdown inside of tomato
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+# Start button
+start_button = Button(text="Start")
+start_button.grid(column=0, row=2)
+
+# Reset button
+reset_button = Button(text="Reset")
+reset_button.grid(column=2, row=2)
+
+# Check display label
+check_label = Label(text="✔️", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15, "bold"))
+check_label.grid(column=1, row=3)
+
+window.mainloop()
