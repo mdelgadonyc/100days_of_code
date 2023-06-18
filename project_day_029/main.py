e48e96e859fce034a58b1cfd7a0b60e3a8c22cb8
mdelgadonyc <moises.f.delgado@gmail.com>
2023-06-17 22:09:05 -0400

    Adds password generate functionality to day 29 project

--- project_day_029/main.py
+++ project_day_029/main.py
@@ -1,10 +1,37 @@
 # Day 29 Project: Password Manager
 
+import random
 import tkinter
 from tkinter import messagebox
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+# Password Generator Project
+def generate_function():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+               'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+               'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
+
+    password_letters = [random.choice(letters) for _ in range(nr_letters)]
+    password_symbols = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers = [random.choice(numbers) for _ in range(nr_numbers)]
+
+    password_list = password_letters + password_symbols + password_numbers
+
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_box.delete(0, "end")
+    password_box.insert(0, password)
+    print(f"Your password is: {password}")
+
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
@@ -15,8 +42,8 @@ def save():
     if len(website) == 0 or len(password) == 0:
         messagebox.showerror(title="Oops", message="Please don't leave any fields empty!")
     else:
-        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\nPassword:"
-                                                              f" {password}\nWould you like to save?")
+        is_ok = messagebox.askokcancel(title=website, message=f"These are the details entered: \nEmail: {email}\n"
+                                                              f"Password: {password}\nWould you like to save?")
 
         # All fields need to be cleared after Add button is pressed.
         website_box.delete(0, 'end')
@@ -61,7 +88,7 @@ user_box.insert(0, "mdelgadonyc@gmail.com")
 
 password_box.grid(column=1, row=3, sticky="EW")
 
-generate_button = tkinter.Button(text="Generate Password")
+generate_button = tkinter.Button(text="Generate Password", command=generate_function)
 add_button = tkinter.Button(text="Add", width=36, command=save)
 
 generate_button.grid(column=2, row=3, sticky="EW")
