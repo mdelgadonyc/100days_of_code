b386bbf677d891f56de986b74ecbe20ad019d1c8
mdelgadonyc <moises.f.delgado@gmail.com>
2023-06-25 15:22:18 -0400

    Adds card flip functionality to Flashy project

--- capstone-003/main.py
+++ capstone-003/main.py
@@ -10,12 +10,28 @@ FILENAME = "data/french_words.csv"
 dataframe = pd.read_csv(FILENAME)
 word_list = dataframe.to_dict(orient="records")
 LANGUAGE = dataframe.columns[0]
+current_word = {}
 
 
 # -------------------------- OBTAIN WORDS ----------------------------- #
-def get_word():
+def next_card():
+    global current_word, flip_timer
+
+    window.after_cancel(flip_timer)
+
     current_word = random.choice(word_list)
-    card_front.itemconfig(word_text, text=current_word[LANGUAGE])
+    canvas.itemconfig(card_image, image=card_front_img)
+    canvas.itemconfig(word_text, text=current_word[LANGUAGE], fill="black")
+    canvas.itemconfig(language, text=LANGUAGE, fill="black")
+
+    flip_timer = window.after(3000, func=flip_card)
+
+
+# --------------------------- FLIP CARD ------------------------------- #
+def flip_card():
+    canvas.itemconfig(card_image, image=card_back_img)
+    canvas.itemconfig(language, text="English", fill="white")
+    canvas.itemconfig(word_text, text=current_word["English"], fill="white")
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -23,25 +39,29 @@ window = tkinter.Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
 
-card_front_img = tkinter.PhotoImage(file="images/card_front.png")
-card_front = tkinter.Canvas(height=526, width=800, highlightthickness=0)
-card_front.config(background=BACKGROUND_COLOR)
-card_front.create_image(413, 270, image=card_front_img)
-card_front.grid(column=0, row=0, columnspan=2)
+flip_timer = window.after(3000, func=flip_card)
 
-language = card_front.create_text(400, 150, text=LANGUAGE, font=("Ariel", 40, "italic"))
-word_text = card_front.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
+card_front_img = tkinter.PhotoImage(file="images/card_front.png")
+card_back_img = tkinter.PhotoImage(file="images/card_back.png")
+
+canvas = tkinter.Canvas(height=526, width=800, highlightthickness=0)
+canvas.config(background=BACKGROUND_COLOR)
+card_image = canvas.create_image(413, 270, image=card_front_img)
+canvas.grid(column=0, row=0, columnspan=2)
+
+language = canvas.create_text(400, 150, text=LANGUAGE, font=("Ariel", 40, "italic"))
+word_text = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 
 # Create the two buttons
 button_wrong_img = tkinter.PhotoImage(file="images/wrong.png")
 button_right_img = tkinter.PhotoImage(file="images/right.png")
 
-button_wrong = tkinter.Button(image=button_wrong_img, highlightthickness=0, command=get_word)
-button_right = tkinter.Button(image=button_right_img, highlightthickness=0, command=get_word)
+button_wrong = tkinter.Button(image=button_wrong_img, highlightthickness=0, command=next_card)
+button_right = tkinter.Button(image=button_right_img, highlightthickness=0, command=next_card)
 
 button_wrong.grid(column=0, row=1)
 button_right.grid(column=1, row=1)
 
-get_word()
+next_card()
 
 window.mainloop()
