8947116ba0f3fea7b270a83c64c9d5e46d4119be
mdelgadonyc <moises.f.delgado@gmail.com>
2023-08-02 14:29:43 -0400

    Completes capstone 5 project

--- capstone-005/post.py
+++ capstone-005/post.py
@@ -1,2 +1,18 @@
+import requests
+
+
 class Post:
-    pass
+    def __init__(self):
+        url = "https://api.npoint.io/d8332566712a9f862efc"
+        response = requests.get(url)
+        response.raise_for_status()
+
+        self.blogs_data = response.json()
+
+    def get_all_posts(self):
+        return self.blogs_data
+
+    def get_post(self, num):
+        # convert post number string to array integer index.
+        num = int(num) - 1
+        return self.blogs_data[num]
