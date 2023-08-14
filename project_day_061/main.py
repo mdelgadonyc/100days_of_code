f6818d073e2d984f37de77cfc7ecf29bea1ef56e
mdelgadonyc <moises.f.delgado@gmail.com>
2023-08-14 14:10:34 -0400

    Adds password length and email format validation for day 61

--- project_day_061/main.py
+++ project_day_061/main.py
@@ -3,13 +3,13 @@ import os
 from flask import Flask, render_template, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, Length
 import os
 
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email', validators=[DataRequired()])
-    password = PasswordField(label='Password', validators=[DataRequired()])
+    email = StringField(label='Email', validators=[DataRequired(), Email()])
+    password = PasswordField(label='Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField(label='Log In')
 
 
