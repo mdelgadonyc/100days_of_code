a102fcd6cc3834bc313db2c057e182f986c8d148
mdelgadonyc <moises.f.delgado@gmail.com>
2023-08-19 01:54:15 -0400

    Commits initial code for day 63 bookshelf project

---
+++ project_day_063/main.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return render_template('index.html', books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        title = request.form.get('title')
+        author = request.form.get('author')
+        rating = int(request.form.get('rating'))
+
+        book_record = {
+            "title": title,
+            "author": author,
+            "rating": rating,
+        }
+        all_books.append(book_record)
+        return redirect(url_for('home'))
+
+    return render_template('add.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
