aaff8dbb21a7191836dfb582705447a444588e34
mdelgadonyc <moises.f.delgado@gmail.com>
2023-08-26 14:21:22 -0400

    Deletes extra comments and keeps db.create_all for when DB doesn't exist

--- project_day_064/main.py
+++ project_day_064/main.py
@@ -38,44 +38,10 @@ class Movie(db.Model):
     ranking = db.Column(db.Integer, nullable=True)
     review = db.Column(db.String(250), nullable=True)
     img_url = db.Column(db.String(250), nullable=False)
-#
-#
-# with app.app_context():
-#     db.create_all()
 
 
-# second_movie = Movie(
-#     title="Avatar The Way of Water",
-#     year=2022,
-#     description="Set more than a decade after the events of the first film, learn the story of the Sully family ("
-#                 "Jake, Neytiri, and their kids), the trouble that follows them, the lengths they go to keep each "
-#                 "other safe, the battles they fight to stay alive, and the tragedies they endure.",
-#     rating=7.3,
-#     ranking=9,
-#     review="I liked the water.",
-#     img_url="https://image.tmdb.org/t/p/w500/t6HIqrRAclMCA60NsSmeqe9RmNV.jpg"
-# )
-#
-# with app.app_context():
-#     db.session.add(second_movie)
-#     db.session.commit()
-
-# one-time code to add the "Phone Booth" demo entry
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an "
-#                 "extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with "
-#                 "the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-#
-# with app.app_context():
-#     db.session.add(new_movie)
-#     db.session.commit()
+with app.app_context():
+    db.create_all()
 
 
 def search_titles(movie_title):
